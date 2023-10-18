2e29889671976cb5942597757900bcea9ec37287
TTimotti <ttimotti@kakao.com>
2023-10-18 10:06:24 +0900

    Main Controller, HTML 생성

--- src/main/java/study/work/signup/controller/MainController.java
+++ src/main/java/study/work/signup/controller/MainController.java
@@ -1,12 +1,13 @@
 package study.work.signup.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping(value="/main")
-    public String mainPage() {
+    public String mainPage(Model model) {
 
         return "main.html";
     }
