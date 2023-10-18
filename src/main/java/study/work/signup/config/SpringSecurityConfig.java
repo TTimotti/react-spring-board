99f8d3a0410a1500d668ec8b5c536e7aeda8da26
TTimotti <ttimotti@kakao.com>
2023-10-18 10:00:30 +0900

    개인 공부 목적 프로젝트 init - 스프링부트, Maven - 프론트 단 미정

---
+++ src/main/java/study/work/signup/config/SpringSecurityConfig.java
@@ -0,0 +1,25 @@
+package study.work.signup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable().authorizeHttpRequests(req -> req.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
