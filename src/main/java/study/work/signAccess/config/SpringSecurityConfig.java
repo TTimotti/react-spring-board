05505a6ffc57562867695982956fadc6af977a65
TTimotti <ttimotti@kakao.com>
2023-11-02 10:51:04 +0900

    시큐리티 관련 코드 수정
    + 로그인 페이지 등록

--- src/main/java/study/work/signAccess/config/SpringSecurityConfig.java
+++ src/main/java/study/work/signAccess/config/SpringSecurityConfig.java
@@ -2,23 +2,41 @@ package study.work.signAccess.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
+@EnableWebSecurity
 public class SpringSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    @Bean // 메소드 이름은 반드시 filterChain 으로 설정
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable().authorizeHttpRequests(req -> req.anyRequest().permitAll()); // CSRF, CORS 기능 OFF
+        http
+            .csrf() // CSRF, CORS 기능 OFF
+                .disable()
+            .cors()
+                .disable()
+            .authorizeHttpRequests()
+                .anyRequest()
+                    .permitAll()
+            .and()
+                .formLogin()
+                    .loginPage("/index");
+
+//        http // https://velog.io/@wooyong99/Spring-Security
+//            .formLogin()
+//                .loginPage("/login")
+
+        ;
+
 
         return http.build();
     }
