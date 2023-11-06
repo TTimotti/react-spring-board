204f0d006abb55f382a5742527969079359d9df8
TTimotti <ttimotti@kakao.com>
2023-11-06 10:57:08 +0900

    Security Config 수정
    6.1 이상 문법으로 설정

--- src/main/java/study/work/sign_access/config/SpringSecurityConfig.java
+++ src/main/java/study/work/sign_access/config/SpringSecurityConfig.java
@@ -1,17 +1,20 @@
 package study.work.sign_access.config;
 
+import io.jsonwebtoken.JwtHandlerAdapter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig {
+@RequiredArgsConstructor
+public class SpringSecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -19,18 +22,14 @@ public class SpringSecurityConfig {
 
     @Bean // 메소드 이름은 반드시 filterChain 으로 설정
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .cors().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-            .formLogin().disable()
-            .httpBasic().disable()
-            .authorizeHttpRequests()
-                .anyRequest()
-                    .permitAll();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authRequest) -> {
+                    authRequest.anyRequest().permitAll();
+                })
+                .formLogin(AbstractHttpConfigurer::disable)
+                .build();
 
-
-        return http.build();
     }
 }
